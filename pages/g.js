9e46168b5b69d9e5226cc305915287e04f9cf498
JuanFCastillo <1006071586@u.icesi.edu.co>
2022-10-31 17:27:47 -0500

    integrate

--- pages/g.js
+++ pages/g.js
@@ -1,8 +1,30 @@
+import Head from "next/head"
+import gal from "../styles/gala.css"
+
 export default function Home() {
+  const [materias] = useState({
+    materias: ["Organizaciones", "Algebra y funciones"]
+  })
   return (
-    <div>
-      <title>New Group</title>
+    <div className={styles.maincontainer}>
+      <Head>
+        <title>New Group</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous"></link>
+      </Head>
+
+
+      <Botton materia={materias.materias}></Botton>
+
+
+
+
+
+      <Transfer></Transfer>
+
+
+
 
     </div>
+
   )
 }
